5c513a55d4842fd7fb6f3f60269d96b828e10f76
Equinet Paul <45755573+EquinetPaul@users.noreply.github.com>
2021-05-03 02:13:07 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,39 @@
+import os.path
+
+rep = "test"
+key = "".encode()
+sizeKey = len(key)
+
+def chunkString(string,key,length):
+    return (list(string[0+i:length+i] for i in range(0, len(string), length)))
+
+def addMissingBytesInList(L,sizeKey):
+    for i, item in enumerate(L):
+        if(len(item)<sizeKey):
+            for _ in range(sizeKey-len(item)):
+                L[i] += (" ").encode()
+    return L
+
+def byte_xor(b):
+    return bytes([_a ^ _b for _a, _b in zip(b, key)])
+
+def concatList(L):
+    concated = bytes()
+    for e in L:
+        concated += e
+    return concated
+
+for root, dirs, files in os.walk(rep, topdown=False):
+   for name in files:
+        link = os.path.join(root, name)
+        sizeFile = os.path.getsize(link)
+        bytesToWrite = bytes()
+        with open(link,"rb") as f:
+            content = f.read()
+            content = chunkString(content,key,sizeKey)
+            cryptedContent = list(map(byte_xor,content))
+            bytesToWrite = concatList(cryptedContent)
+        f.close()
+        with open(link,"wb") as f:
+            f.write(bytesToWrite)
+        f.close()
