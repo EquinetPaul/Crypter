4da0c52d8572a0e01a68c0a6d52e75b4ef737376
Equinet Paul <45755573+EquinetPaul@users.noreply.github.com>
2021-05-04 03:42:38 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,37 +1,36 @@
 import os.path
+import time
 
-rep = "test"
+start = time.time()
+rep = "test/"
 key = "".encode()
 sizeKey = len(key)
+count = 0
 
-def chunkString(string,key,length):
+def chunkString(string,length):
     return (list(string[0+i:length+i] for i in range(0, len(string), length)))
-
-def addMissingBytesInList(L,sizeKey):
-    for i, item in enumerate(L):
-        if(len(item)<sizeKey):
-            for _ in range(sizeKey-len(item)):
-                L[i] += (" ").encode()
-    return L
 
 def byte_xor(b):
     return bytes([_a ^ _b for _a, _b in zip(b, key)])
 
-def concatList(L):
-    concated = bytes()
-    for e in L:
-        concated += e
-    return concated
+def total_time(start,end):
+    return(end-start)
 
 for root, dirs, files in os.walk(rep, topdown=False):
    for name in files:
+        print("Processing...")
+        count+=1
         link = os.path.join(root, name)
         sizeFile = os.path.getsize(link)
         with open(link,"rb") as f:
             content = f.read()
-            content = chunkString(content,key,sizeKey)
+            content = chunkString(content,sizeKey)
             cryptedContent = list(map(byte_xor,content))
-        f.close()
         with open(link,"wb") as f:
-            map(f.write,cryptedContent)
-        f.close()
+            for e in cryptedContent:
+                f.write(e)
+            # map(f.write,cryptedContent)
+
+print("END")
+print(total_time(start,time.time()))
+print(str(count)+" files crypted.")
